42e59b8d93ba30d34ce25973ce8ed685be188216
jj2016025j <103946700+jj2016025j@users.noreply.github.com>
2024-01-11 16:29:29 +0800

    小修正

--- static/javaScript/frontSide.js
+++ static/javaScript/frontSide.js
@@ -224,67 +224,67 @@ function createHeader() {
 function createFooter() {
     const footer = document.getElementsByTagName('footer')[0];
     footer?.classList.add("bg-black", "text-white", "justify-content-center");
+    //  <a href="https://twitter.com/LEECS871116"><i class="bi bi-twitter"></i></a>
     if (footer) {
         footer.innerHTML = `
-    <div class="col-12 row justify-content-center mx-auto">
-        <div class="col-md-6 col-12 d-flex justify-content-around mt-3">
-            <a href="https://github.com/jj2016025j"><i class="bi bi-github"></i></a>
-            <a href="https://www.facebook.com/lee871116/"><i class="bi bi-facebook" size="2x"></i></a>            
-            <a href="https://www.instagram.com/lee_871116"><i class="bi bi-instagram"></i></a>           
-            <a href="https://twitter.com/LEECS871116"><i class="bi bi-twitter"></i></a>
-            <a href="https://www.youtube.com/channel/UCdFWtSFizZFpBovsUY7ZKyg"><i class="bi bi-youtube"></i></a>
-            <a href="https://www.behance.net/d71ea9ce"><i class="bi bi-behance"></i></a>
-            <a href="https://www.linkedin.com/in/%E5%BB%BA%E9%99%9E-%E6%9D%8E-3145931b2/"><i class="bi bi-linkedin"></i></a>
-            <a href="linkfly.to/leecs871116"><i class="bi bi-link"></i></a>
-            <a href="https://leejohnson.ddns.net/templates/"><i class="bi bi-browser-chrome"></i></a>
-        </div>
-        <div class="col-md-6 col-12 d-flex justify-content-around text-center mt-3">
-            <a href="https://www.canva.com/design/DAF4PrNLaIc/XBZqJm5EFzzmeLM4V6hkFQ/edit?ui=eyJHIjp7fX0" target="_blank"
-            rel="noopener noreferrer">
-                隱私權政策</a>
-            <a href="https://www.canva.com/design/DAF4PrNLaIc/XBZqJm5EFzzmeLM4V6hkFQ/edit?utm_content=DAF4PrNLaIc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target="_blank"
-            rel="noopener noreferrer">
-                使用者協議</a>
-            <a href="./about.html" target="_blank" rel="noopener noreferrer">
-                公司簡介</a>
-            <a href="mailto:jj2016025j@gmail.com.com" target="_blank" rel="noopener noreferrer">
-                聯絡我們</a>
-        </div>
-    </div>
-    <br>
-    <div class="col-12 row text-center justify-content-center mx-auto">
-        <div class="col-md-5">
-            <p>
-                本網站所提供的所有產品與服務資訊，包括但不限於價格、描述、可用性及性能等，
-                均“如其所是”地提供，且不含任何形式的保證或聲明。我們不對任何產品或服務的準確性、
-                可靠性或適用性作出任何保證。使用這些產品或服務所產生的風險完全由用戶自行承擔。
-            </p>
-            <p>
-                All product and service information provided on this website, 
-                including but not limited to prices, descriptions, availability, 
-                and performance, are provided "as is" without any form of guarantee or representation. 
-                We make no guarantees regarding the accuracy, reliability, 
-                or applicability of any product or service. 
-                The risk associated with the use of these products or services is entirely borne by the user.
-            </p>
-        </div>
-        <div class="col-md-5 d-none d-sm-block">
-            <p>
-                本網站中的所有內容，包括文字、圖形、標誌、按鈕圖標、圖像、音頻剪輯、數位下載、數據編輯和軟件，
-                均為網站擁有者或其內容提供者的財產，並受國際版權法的保護。本網站的所有內容僅用於信息和個人非商業用途，
-                在未經明確的書面許可下，不得以任何形式複製、再發布、上傳、發布、傳播或分發。
-            </p>
-            <p>
-                All content on this website, including text, graphics, logos, 
-                button icons, images, audio clips, digital downloads, data compilations, and software, 
-                is the property of the website owner or its content providers and is protected by international copyright laws. 
-                The content of this website is intended for informational and personal non-commercial use only and may not be reproduced, 
-                republished, uploaded, posted, transmitted, or distributed in any form without explicit written permission.
-            </p>
-        </div>
-        <a href="https://www.canva.com/design/DAF4PrNLaIc/XBZqJm5EFzzmeLM4V6hkFQ/edit?utm_content=DAF4PrNLaIc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" class="mb-5">Copyright © 2023 建陞教練的工作室 All Rights Reserved.</a>
-    </div>
-    `;
+            <div class="col-12 row justify-content-center mx-auto">
+                <div class="col-md-6 col-12 d-flex justify-content-around mt-3">
+                    <a href="https://github.com/jj2016025j"><i class="bi bi-github"></i></a>
+                    <a href="https://www.facebook.com/lee871116/"><i class="bi bi-facebook" size="2x"></i></a>            
+                    <a href="https://www.instagram.com/lee_871116"><i class="bi bi-instagram"></i></a>           
+                    <a href="https://www.youtube.com/channel/UCdFWtSFizZFpBovsUY7ZKyg"><i class="bi bi-youtube"></i></a>
+                    <a href="https://www.behance.net/d71ea9ce"><i class="bi bi-behance"></i></a>
+                    <a href="https://www.linkedin.com/in/%E5%BB%BA%E9%99%9E-%E6%9D%8E-3145931b2/"><i class="bi bi-linkedin"></i></a>
+                    <a href="linkfly.to/leecs871116"><i class="bi bi-link"></i></a>
+                    <a href="https://leejohnson.ddns.net/templates/"><i class="bi bi-browser-chrome"></i></a>
+                </div>
+                <div class="col-md-6 col-12 d-flex justify-content-around text-center mt-3">
+                    <a href="https://www.canva.com/design/DAF4PrNLaIc/XBZqJm5EFzzmeLM4V6hkFQ/edit?ui=eyJHIjp7fX0" target="_blank"
+                    rel="noopener noreferrer">
+                        隱私權政策</a>
+                    <a href="https://www.canva.com/design/DAF4PrNLaIc/XBZqJm5EFzzmeLM4V6hkFQ/edit?utm_content=DAF4PrNLaIc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" target="_blank"
+                    rel="noopener noreferrer">
+                        使用者協議</a>
+                    <a href="./about.html" target="_blank" rel="noopener noreferrer">
+                        公司簡介</a>
+                    <a href="mailto:jj2016025j@gmail.com.com" target="_blank" rel="noopener noreferrer">
+                        聯絡我們</a>
+                </div>
+            </div>
+            <br>
+            <div class="col-12 row text-center justify-content-center mx-auto">
+                <div class="col-md-5">
+                    <p>
+                        本網站所提供的所有產品與服務資訊，包括但不限於價格、描述、可用性及性能等，
+                        均“如其所是”地提供，且不含任何形式的保證或聲明。我們不對任何產品或服務的準確性、
+                        可靠性或適用性作出任何保證。使用這些產品或服務所產生的風險完全由用戶自行承擔。
+                    </p>
+                    <p>
+                        All product and service information provided on this website, 
+                        including but not limited to prices, descriptions, availability, 
+                        and performance, are provided "as is" without any form of guarantee or representation. 
+                        We make no guarantees regarding the accuracy, reliability, 
+                        or applicability of any product or service. 
+                        The risk associated with the use of these products or services is entirely borne by the user.
+                    </p>
+                </div>
+                <div class="col-md-5 d-none d-sm-block">
+                    <p>
+                        本網站中的所有內容，包括文字、圖形、標誌、按鈕圖標、圖像、音頻剪輯、數位下載、數據編輯和軟件，
+                        均為網站擁有者或其內容提供者的財產，並受國際版權法的保護。本網站的所有內容僅用於信息和個人非商業用途，
+                        在未經明確的書面許可下，不得以任何形式複製、再發布、上傳、發布、傳播或分發。
+                    </p>
+                    <p>
+                        All content on this website, including text, graphics, logos, 
+                        button icons, images, audio clips, digital downloads, data compilations, and software, 
+                        is the property of the website owner or its content providers and is protected by international copyright laws. 
+                        The content of this website is intended for informational and personal non-commercial use only and may not be reproduced, 
+                        republished, uploaded, posted, transmitted, or distributed in any form without explicit written permission.
+                    </p>
+                </div>
+                <a href="https://www.canva.com/design/DAF4PrNLaIc/XBZqJm5EFzzmeLM4V6hkFQ/edit?utm_content=DAF4PrNLaIc&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton" class="mb-5">Copyright © 2023 建陞教練的工作室 All Rights Reserved.</a>
+            </div>
+            `;
     }
 }
 
